757c393d78bcb8ae56018f0ec83d89ad5d4a6cca
Home <Home@DESKTOP-F8NUUGM>
2023-04-06 11:39:40 -0300

    criando um olá mundo com spring-boot

--- vendas/src/main/java/projetoVendas/VendasApplication.java
+++ vendas/src/main/java/projetoVendas/VendasApplication.java
@@ -2,9 +2,25 @@ package projetoVendas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VendasApplication {
+	
+	@GetMapping("/hello")
+	public String helloWord() {
+		
+		
+		return " hello Word ";
+	}
+	
+	@GetMapping("/mensagem")
+	
+	public String Mensagem() {
+		return " Olá Usuario Seja bem vindo ao Spring Boot";
+	}
 	
 	public static void main(String[] args) {
 		
