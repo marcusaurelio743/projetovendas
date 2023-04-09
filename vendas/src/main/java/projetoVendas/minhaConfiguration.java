81c8db4f319140a538766e2e73ff256969437d1c
Home <Home@DESKTOP-F8NUUGM>
2023-04-08 23:03:00 -0300

    configurando multiplos ambiente e o profile do spring boot

--- vendas/src/main/java/projetoVendas/minhaConfiguration.java
+++ vendas/src/main/java/projetoVendas/minhaConfiguration.java
@@ -1,9 +1,19 @@
 package projetoVendas;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("developement")
 public class minhaConfiguration {
+	@Bean
+	public CommandLineRunner execultar() {
+		return args ->{
+			System.out.println("Rodando a configuração de desenvolvimento");
+		};
+	}
 	
 	
 
