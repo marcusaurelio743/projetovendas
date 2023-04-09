2648b15948410008eaff74ace9427e0e132f4f72
Home <Home@DESKTOP-F8NUUGM>
2023-04-08 23:44:50 -0300

    criando anotações customizadas no spring

--- vendas/src/main/java/projetoVendas/minhaConfiguration.java
+++ vendas/src/main/java/projetoVendas/minhaConfiguration.java
@@ -2,10 +2,9 @@ package projetoVendas;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Configuration
+@Development 
 @Profile("developement")
 public class minhaConfiguration {
 	@Bean
